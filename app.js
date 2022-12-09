0cd0a7b9b1b23034e32433578137e36eab609ed8
Ankidt <117918783+Ankidt@users.noreply.github.com>
2022-12-09 14:08:01 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const contents = document.querySelector(`content`)
-const listItems = document.querySelector(`nav ul li`)
-listItems.forEach((Item,idx) => {
-    item.addEventlistener(`click`,()=>{
+const contents = document.querySelectorAll(`content`)
+const listItems = document.querySelectorAll(`nav ul li`)
+
+listItems.forEach((item,idx) => {
+    item.addEventListener(`click`,()=>{
         hideAllContents()
         hideAllItems()
 
